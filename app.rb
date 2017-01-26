f955828c4ac3947086635e2bf1e0c8aec45d79b5
Francisco <franciscohuertas38@gmail.com>
2017-01-26 09:34:24 -0500

    Implementada funcion creacion de tareas - completada estructura del proyecto

--- app.rb
+++ app.rb
@@ -1,2 +1,13 @@
 require 'sinatra'
 require 'make_todo'
+
+get '/' do
+	@notes = Tarea.all
+ 	@title = 'Lista de cosas por hacer - App'
+  	erb :home
+end  
+
+post '/' do
+  Tarea.create(params[:content].capitalize)
+  redirect '/'
+end
