e1e4bddf5b756e8f02dccc382a37a6f0f4a0d898
Francisco <franciscohuertas38@gmail.com>
2017-01-26 09:43:37 -0500

    Implementada funcion eliminar y completar tareas

--- app.rb
+++ app.rb
@@ -11,3 +11,13 @@ post '/' do
   Tarea.create(params[:content].capitalize)
   redirect '/'
 end
+
+get '/:id/delete' do
+  Tarea.destroy(params[:id])
+  redirect '/'
+end
+
+get '/:id/complete' do
+  Tarea.update(params[:id])
+  redirect '/'
+end
